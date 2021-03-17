448c0eec8ce24604de1a96c091a68995d92c84f5
mona lisa <monalisa58979@gmail.com>
2021-03-18 02:34:35 +0900

    メモ化をしてアルゴリズムを改善

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-'use strict';
+`use strict`;
+
+//40まで出力するという前提
+
+var memo = new Map(); 
+memo.set(0,0)
+memo.set(1,1)
+
+function fib(n){
+  if(memo.has(n)){
+    return memo.get(n);
+  }else{
+    const value = fib(n-1)+fib(n-2);
+    memo.set(n,value); //ここポイント！
+    return value;
+   } 
+}
+
+
+const length = 40;
+
+for(let i = 0; i <= length; i++){
+  console.log(fib(i)+ ",");
+}
+
+
+
+
+
